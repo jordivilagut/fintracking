8c926205362d9a01b85a4c1913a9ba5b0cbf4c3d
jordivilagut <vilagutjordi@gmail.com>
2020-12-30 18:03:28 +0100

    Add more expense types

--- src/main/kotlin/com/jordivilagut/fintracking/model/dto/ExpenseType.kt
+++ src/main/kotlin/com/jordivilagut/fintracking/model/dto/ExpenseType.kt
@@ -2,5 +2,19 @@ package com.jordivilagut.fintracking.model.dto
 
 enum class ExpenseType {
     CLOTHING,
-    EDUCATION
+    EDUCATION,
+    TRANSPORT,
+    TELCO,
+    TRAVELS,
+    STREAMING_PLATFORMS,
+    PERSONAL_CARE,
+    GROCERIES,
+    RESTAURANTS,
+    HEALTHCARE,
+    SALARY,
+    TAXES,
+    CHARITY,
+    HOUSING,
+    HOUSEHOLD_SUPPLIES,
+    OTHER
 }
