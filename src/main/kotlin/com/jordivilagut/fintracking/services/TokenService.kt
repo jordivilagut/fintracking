7f12110b24fa5c7a1dfc614a92884686193f8129
jordivilagut <vilagutjordi@gmail.com>
2021-01-05 19:49:40 +0100

    Increase token expiration to one month

--- src/main/kotlin/com/jordivilagut/fintracking/services/TokenService.kt
+++ src/main/kotlin/com/jordivilagut/fintracking/services/TokenService.kt
@@ -9,7 +9,7 @@ interface TokenService {
         const val SUBJECT: String = "token-subject"
         const val ISSUER: String = "issuer-name"
         const val SECRET_KEY = "oeRaYY7Wo24sDqKSX3IM9ASGmdGPmkTd9jo1QTy4b7P9Ze5_9hKolVX8xNrQDcNRfVEdTZNOuOyqEGhXEbdJI-ZQ19k_o9MI0y3eZN2lp9jow55FfXMiINEdt1XR85VipRLSOkT6kSpzs2x-jbLDiz9iFVzkd81YKxMgPA7VfZeQUm4n-mOmnWMaVX30zGFU4L3oPBctYKkl4dYfqYWqRNfrgPJVi5DGFjywgxx0ASEiJHtV72paI3fDR2XwlSkyhhmY-ICjCRmsJN4fX1pdoL8a18-aQrvyu4j0Os6dVPYIoPvvY0SAZtWYKHfM15g7A3HD4cVREf9cUsprCRK93w"
-        const val EXPIRATION_TIME: Long = 604800000 //One week
+        const val EXPIRATION_TIME: Long = 2_592_000_000 //One month
     }
 
     fun createJWT(user: User): String
